3ecc56cabc66dc36cc7c5717d5db0033199d4a75
narayana <mandavalakshminarayana446@gmail.com>
2025-10-17 22:42:31 +0530

    Updated pom.xml

--- springbootbackend/src/main/java/com/klef/dev/SpringbootbackendApplication.java
+++ springbootbackend/src/main/java/com/klef/dev/SpringbootbackendApplication.java
@@ -2,6 +2,7 @@ package com.klef.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringbootbackendApplication extends SpringBootServletInitializer
